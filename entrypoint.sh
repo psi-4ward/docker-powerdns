d2a083c06d5a1d2bddd159c9d8c74c42321c7ad3
tcely <tcely@users.noreply.github.com>
2017-07-29 18:03:57 -0400

    Optimized entrypoint.sh
    Check MYSQL_AUTOCONF value, don't execute arbitrary commands.
    There were several quoting problems.
    Also, a few silly things were being done.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,51 +2,53 @@
 set -e
 
 # --help, --version
-[ "$1" = "--help" ] || [ "$1" = "--version" ] && exec pdns_server $1
+[ "$1" = '--help' ] || [ "$1" = '--version' ] && exec pdns_server "$1"
 # treat everything except -- as exec cmd
-[ "${1:0:2}" != "--" ] && exec "$@"
+[ "${1:0:2}" != '--' ] && exec "$@"
 
 # Set MySQL Credentials in pdns.conf
-if $MYSQL_AUTOCONF ; then
-  sed -r -i "s/^[# ]*gmysql-host=.*/gmysql-host=${MYSQL_HOST}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-port=.*/gmysql-port=${MYSQL_PORT}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-user=.*/gmysql-user=${MYSQL_USER}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-password=.*/gmysql-password=${MYSQL_PASS}/g" /etc/pdns/pdns.conf
-  sed -r -i "s/^[# ]*gmysql-dbname=.*/gmysql-dbname=${MYSQL_DB}/g" /etc/pdns/pdns.conf
+if [ "$MYSQL_AUTOCONF" = 'true' ]; then
+  MYSQL_PASSWORD="$MYSQL_PASS" MYSQL_DBNAME="$MYSQL_DB"
+  SEDPROG='/^[# ]*gmysql-/{';
+  for _var in host port user password dbname; do
+    _mysqlvar='${MYSQL_'"$(echo "$_var" | tr '[a-z]' '[A-Z]')"'}'
+    _mysqlvarvalue="$(eval echo \""$_mysqlvar"\")"
+
+    SEDPROG="${SEDPROG}$(printf 's/gmysql-%s=.*/gmysql-%s=%s/g;' "$_var" "$_var" "$_mysqlvarvalue")"
+  done; unset -v _var _mysqlvar _mysqlvarvalue
+  SEDPROG="${SEDPROG}"'};'
+  unset -v MYSQL_PASSWORD MYSQL_DBNAME
+  
+  sed -r -i "$SEDPROG" /etc/pdns/pdns.conf
+  unset -v SEDPROG
 fi
 
-
-MYSQLCMD="mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASS -r -N"
+MYSQLCMD="mysql -h '${MYSQL_HOST}' -u '${MYSQL_USER}' -p '${MYSQL_PASS}' -r -N"
 
 # wait for Database come ready
 isDBup () {
-  echo "SHOW STATUS" | $MYSQLCMD 1>/dev/null
-  echo $?
+  echo 'SHOW STATUS' | eval "$MYSQLCMD" 1>/dev/null
 }
 
 RETRY=10
-until [ `isDBup` -eq 0 ] || [ $RETRY -le 0 ] ; do
+until isDBup || [ $(( RETRY-- )) -le 0 ] ; do
   echo "Waiting for database to come up"
   sleep 5
-  RETRY=$(expr $RETRY - 1)
 done
 if [ $RETRY -le 0 ]; then
-  >&2 echo Error: Could not connect to Database on $MYSQL_HOST:$MYSQL_PORT
+  1>&2 echo "Error: Could not connect to Database on ${MYSQL_HOST}:${MYSQL_PORT}"
   exit 1
 fi
 
 # init database if necessary
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;" | $MYSQLCMD
-MYSQLCMD="$MYSQLCMD $MYSQL_DB"
+printf 'CREATE DATABASE IF NOT EXISTS "%s";\n' "$MYSQL_DB" | eval "$MYSQLCMD"
+MYSQLCMD="${MYSQLCMD} ${MYSQL_DB}"
 
-if [ "$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = \"$MYSQL_DB\";" | $MYSQLCMD)" -le 1 ]; then
-  echo Initializing Database
-  cat /etc/pdns/schema.sql | $MYSQLCMD
+if [ "$(printf 'SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = "%s";\n' "$MYSQL_DB" | eval "$MYSQLCMD")" -le 1 ]; then
+  echo 'Initializing Database'
+  < /etc/pdns/schema.sql eval "$MYSQLCMD"
 fi
 
 trap "pdns_control quit" SIGHUP SIGINT SIGTERM
 
-pdns_server "$@" &
-
-wait
-
+pdns_server "$@"
