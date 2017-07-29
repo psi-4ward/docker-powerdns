c4a41351c1e85bbb009fb5917a1c6a4cb4cb398c
tcely <tcely@users.noreply.github.com>
2017-07-29 13:05:33 -0400

    Typo: expect -> except

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # --help, --version
 [ "$1" = "--help" ] || [ "$1" = "--version" ] && exec pdns_server $1
-# treat everything expect -- as exec cmd
+# treat everything except -- as exec cmd
 [ "${1:0:2}" != "--" ] && exec "$@"
 
 # Set MySQL Credentials in pdns.conf
