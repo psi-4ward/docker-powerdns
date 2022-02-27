c3200d1d28a54c8444ca493a294319eabb250050
Oliver Schaal <oliver.schaal@lostsignal.info>
2022-02-27 23:11:00 +0100

    entrypoint: refactoring script

--- README.md
+++ README.md
@@ -1,5 +1,90 @@
 # PowerDNS Docker Container
 
+Build of https://hub.docker.com/r/psitrax/powerdns - https://github.com/psi-4ward/docker-powerdns
+
+Description for dev-build below: [PowerDNS Docker Container DevBuild](#powerdns-docker-container-devbuild)
+
+# PowerDNS Docker Container - Old Build 4.6
+
+[![Docker Stars](https://img.shields.io/docker/stars/psitrax/powerdns.svg)](https://hub.docker.com/r/psitrax/powerdns/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/psitrax/powerdns.svg)](https://hub.docker.com/r/psitrax/powerdns/)
+[![Docker Automated buil](https://img.shields.io/docker/automated/psitrax/powerdns.svg)](https://hub.docker.com/r/psitrax/powerdns/)
+
+* Small Alpine based Image
+* MySQL (default), Postgres, SQLite and Bind backend included
+* DNSSEC support optional
+* Automatic MySQL database initialization
+* Latest PowerDNS version (if not pls file an issue)
+* Guardian process enabled
+* Graceful shutdown using pdns_control
+
+## Supported tags
+
+* Exact: i.e. `v4.4.1`: PowerDNS Version 4.4.1
+* `v4.0`: PowerDNS Version 4.0.x, latest image build
+* `v4`: PowerDNS Version 4.x.x, latest image build
+
+Note: Some older tags don't have the `v` at the beginning (e.g. for Version 4.1.7 the tag is `4.1.7`)
+
+## Usage
+
+```shell
+# Start a MySQL Container
+$ docker run -d \
+  --name pdns-mysql \
+  -e MYSQL_ROOT_PASSWORD=supersecret \
+  -v $PWD/mysql-data:/var/lib/mysql \
+  mariadb:10.1
+
+$ docker run --name pdns \
+  --link pdns-mysql:mysql \
+  -p 53:53 \
+  -p 53:53/udp \
+  -e MYSQL_USER=root \
+  -e MYSQL_PASS=supersecret \
+  -e MYSQL_PORT=3306 \
+  psitrax/powerdns \
+    --cache-ttl=120 \
+    --allow-axfr-ips=127.0.0.1,123.1.2.3
+```
+
+## Configuration
+
+**Environment Configuration:**
+
+* MySQL connection settings
+  * `MYSQL_HOST=mysql`
+  * `MYSQL_USER=root`
+  * `MYSQL_PASS=root`
+  * `MYSQL_DB=pdns`
+  * `MYSQL_DNSSEC=no`
+* To support docker secrets, use same variables as above with suffix `_FILE`.
+* Want to disable mysql initialization? Use `MYSQL_AUTOCONF=false`
+* DNSSEC is disabled by default, to enable use `MYSQL_DNSSEC=yes`
+* Want to use own config files? Mount a Volume to `/etc/pdns/conf.d` or simply overwrite `/etc/pdns/pdns.conf`
+
+**PowerDNS Configuration:**
+
+Append the PowerDNS setting to the command as shown in the example above.
+See `docker run --rm psitrax/powerdns --help`
+
+
+## License
+
+[GNU General Public License v2.0](https://github.com/PowerDNS/pdns/blob/master/COPYING) applyies to PowerDNS and all files in this repository.
+
+
+## Maintainer
+
+* Christoph Wiechert <wio@psitrax.de>
+
+### Credits
+
+* Mathias Kaufmann <me@stei.gr>: Reduced image size
+
+
+# PowerDNS Docker Container DevBuild
+
 [![Docker Stars](https://img.shields.io/docker/stars/cybiox9/powerdns.svg)](https://hub.docker.com/r/cyviox9/powerdns/)
 [![Docker Pulls](https://img.shields.io/docker/pulls/cybiox9/powerdns.svg)](https://hub.docker.com/r/cybiox9/powerdns/)
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 [[ -z "$TRACE" ]] || set -x
 
+# helper for docker setup env from file ----------------------------------------
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
@@ -26,7 +27,7 @@ file_env() {
     unset "$fileVar"
 }
 
-# Loads various settings that are used elsewhere in the script
+# Loads various settings that are used elsewhere in the script -----------------
 docker_setup_env() {
     # Initialize values that might be stored in a file
 
@@ -52,17 +53,205 @@ docker_setup_env() {
 
 }
 
-docker_setup_env
+# sqlite db --------------------------------------------------------------------
+init_sqlite3() {
+  if [[ ! -f "$PDNS_GSQLITE3_DATABASE" ]]; then
+    echo "Initializing ${PDNS_GSQLITE3_DATABASE}"
+    install -D -d -o pdns -g pdns -m 0755 $(dirname $PDNS_GSQLITE3_DATABASE)
+    cat /etc/pdns/sql/schema.sqlite3.sql | sqlite3 ${PDNS_GSQLITE3_DATABASE}
+    chown pdns:pdns $PDNS_GSQLITE3_DATABASE
+    INITIAL_DB_VERSION=$SQLITE_VERSION
+  fi
+  if [ "$AUTO_SCHEMA_MIGRATION" == "yes" ]; then
+    # init version database if necessary
+    if [[ "$(echo "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='$SCHEMA_VERSION_TABLE';" | sqlite3 ${PDNS_GSQLITE3_DATABASE})" -eq 0 ]]; then
+      [ -z "$INITIAL_DB_VERSION" ] && >&2 echo "Error: INITIAL_DB_VERSION is required when you use AUTO_SCHEMA_MIGRATION for the first time" && exit 1
+      echo "CREATE TABLE $SCHEMA_VERSION_TABLE (id INTEGER PRIMARY KEY, version VARCHAR(255) NOT NULL)" | sqlite3 ${PDNS_GSQLITE3_DATABASE}
+      echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$INITIAL_DB_VERSION');" | sqlite3 ${PDNS_GSQLITE3_DATABASE}
+      echo "Initialized schema version to $INITIAL_DB_VERSION"
+    fi
+    # do the database upgrade
+    while true; do
+      current="$(echo "SELECT version FROM $SCHEMA_VERSION_TABLE ORDER BY id DESC LIMIT 1;" | sqlite3 ${PDNS_GSQLITE3_DATABASE})"
+      if [ "$current" != "$SQLITE_VERSION" ]; then
+        filename=/etc/pdns/sql/${current}_to_*_schema.sqlite3.sql
+        echo "Applying Update $(basename $filename)"
+        sqlite3 ${PDNS_GSQLITE3_DATABASE} < $filename
+        current=$(basename $filename | sed -n 's/^[0-9.]\+_to_\([0-9.]\+\)_.*$/\1/p')
+        echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$current');" | sqlite3 ${PDNS_GSQLITE3_DATABASE}
+      else
+        break
+      fi
+    done
+  fi
+}
+
+# mysql db ---------------------------------------------------------------------
+# MYSQLCMD="mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASS -r -N"
+mysql_connect_db() {
+  mysql -h ${PDNS_GMYSQL_HOST} -u ${PDNS_GMYSQL_USER} -p${PDNS_GMYSQL_PASSWORD}
+  if [ $? -eq 0 ]; then
+    echo "Connection attempt successful"
+    MYSQL_CONNECTION_SUCCESS=1
+  else
+    echo "waiting 5 seconds before next attempt"
+    sleep 5
+  fi
+}
+
+mysql_query() {
+  local MYSQL_QUERY=${1}
+  mysql -r -N -h ${PDNS_GMYSQL_HOST} -u ${PDNS_GMYSQL_USER} -p${PDNS_GMYSQL_PASSWORD} \
+    -e "${MYSQL_QUERY}" ${PDNS_GMYSQL_DBNAME}
+}
+
+mysql_import_schema() {
+  pdns_tables=$(mysql_query "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '${PDNS_GMYSQL_DBNAME}';")
+  if [ ${pdns_tables} -lt 1 ]; then
+    echo "Initializing Mysql database schema"
+    mysql -r -N -h ${PDNS_GMYSQL_HOST} -u ${PDNS_GMYSQL_USER} -p${PDNS_GMYSQL_PASSWORD} \
+      ${PDNS_GMYSQL_DBNAME} < /etc/pdns/schemas/schema.mysql.sql
+  fi
+}
+
+init_mysql() {
+  if [ -z "${PDNS_GMYSQL_HOST}" ] || [ -z "${PDNS_GMYSQL_USER}" ] || [ -z "${PDNS_GMYSQL_PASSWORD}" ] || [ -z "${PDNS_GMYSQL_DBNAME}" ]; then
+    echo '!! Initializing of mysql backend cannot complete without all the required env variables set: '
+    echo '   MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB'
+  else
+    MYSQLCMD="mysql -h $PDNS_GMYSQL_HOST -u $PDNS_GMYSQL_USER -p$PDNS_GMYSQL_PASSWORD -r -N"
+    echo "Attempt connection to mysql server: ${PDNS_GMYSQL_HOST} ..."
+    ATTEMPTS_REMAINING=12
+    MYSQL_CONNECTION_SUCCESS=0
+    until [ ${MYSQL_CONNECTION_SUCCESS} -gt 0 ] || [ ${ATTEMPTS_REMAINING} -le 0 ]; do
+      echo "Connection attempts remaining: ${ATTEMPTS_REMAINING}"
+      mysql_connect_db
+      ATTEMPTS_REMAINING=$(expr ${ATTEMPTS_REMAINING} - 1)
+    done
+    if [ ${ATTEMPTS_REMAINING} -le 0 ]; then
+      echo "!! Exiting: Exhausted attempts to connect to mysql server. (${PDNS_GMYSQL_HOST}:${PDNS_GMYSQL_PORT})"
+      exit 1
+    fi
+    echo "Creating database (if it does not already exist)"
+    mysql_query "CREATE DATABASE IF NOT EXISTS ${PDNS_GMYSQL_DBNAME};"
+    mysql_import_schema
+    INITIAL_DB_VERSION=$MYSQL_VERSION
+  fi
+
+  MYSQLCMD="$MYSQLCMD $PDNS_GMYSQL_DBNAME"
+  if [ "$AUTO_SCHEMA_MIGRATION" == "yes" ]; then
+    # init version database if necessary
+    if [ "$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = \"${PDNS_GMYSQL_DBNAME}\" and table_name = \"$SCHEMA_VERSION_TABLE\";" | $MYSQLCMD)" -eq 0 ]; then
+      [ -z "$INITIAL_DB_VERSION" ] && >&2 echo "Error: INITIAL_DB_VERSION is required when you use AUTO_SCHEMA_MIGRATION for the first time" && exit 1
+      echo "CREATE TABLE $SCHEMA_VERSION_TABLE (id INT AUTO_INCREMENT primary key NOT NULL, version VARCHAR(255) NOT NULL) Engine=InnoDB CHARACTER SET 'latin1';" | $MYSQLCMD
+      echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$INITIAL_DB_VERSION');" | $MYSQLCMD
+      echo "Initialized schema version to $INITIAL_DB_VERSION"
+    fi
+    # do the database upgrade
+    while true; do
+      current="$(echo "SELECT version FROM $SCHEMA_VERSION_TABLE ORDER BY id DESC LIMIT 1;" | $MYSQLCMD)"
+      if [ "$current" != "$MYSQL_VERSION" ]; then
+        filename=/etc/pdns/sql/${current}_to_*_schema.mysql.sql
+        echo "Applying Update $(basename $filename)"
+        $MYSQLCMD < $filename
+        current=$(basename $filename | sed -n 's/^[0-9.]\+_to_\([0-9.]\+\)_.*$/\1/p')
+        echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$current');" | $MYSQLCMD
+      else
+        break
+      fi
+    done
+  fi
+  # Run custom mysql post-init sql scripts
+  if [ -d "/etc/pdns/mysql-postinit" ]; then
+    for SQLFILE in $(ls -1 /etc/pdns/mysql-postinit/*.sql | sort) ; do
+      echo Source $SQLFILE
+      cat $SQLFILE | $MYSQLCMD
+    done
+  fi
+}
+
+# postgresql db ----------------------------------------------------------------
+# PGSQLCMD="psql --host=$PGSQL_HOST --username=$PGSQL_USER"
+pgsql_connect_db() {
+  psql --host=$PDNS_GPGSQL_HOST --username=$PDNS_GPGSQL_USER
+  if [ $? -eq 0 ]; then
+    echo "Connection attempt successful"
+    PGSQL_CONNECTION_SUCCESS=1
+  else
+    echo "waiting 5 seconds before next attempt"
+    sleep 5
+  fi
+}
+
+init_pgsql () {
+  if [ -z "${PDNS_GPGSQL_HOST}" ] || [ -z "${PDNS_GPGSQL_USER}" ] || [ -z "${PDNS_GPGSQL_PASSWORD}" ] || [ -z "${PDNS_GPGSQL_DBNAME}" ]; then
+    echo '!! Initializing of mysql backend cannot complete without all the required env variables set: '
+    echo '   PGSQL_HOST, PGSQL_USER, PGSQL_PASSWORD, PGSQL_DB'
+  else
+    PGSQLCMD="psql --host=$PDNS_GPGSQL_HOST --username=$PDNS_GPGSQL_USER"
+    echo "Attempt connection to pgsql server: ${PDNS_GPGSQL_HOST} ..."
+    ATTEMPTS_REMAINING=12
+    PGSQL_CONNECTION_SUCCESS=0
+    until [ ${PGSQL_CONNECTION_SUCCESS} -gt 0 ] || [ ${ATTEMPTS_REMAINING} -le 0 ]; do
+      echo "Connection attempts remaining: ${ATTEMPTS_REMAINING}"
+      pgsql_connect_db
+      ATTEMPTS_REMAINING=$(expr ${ATTEMPTS_REMAINING} - 1)
+    done
+    if [ ${ATTEMPTS_REMAINING} -le 0 ]; then
+      echo "!! Exiting: Exhausted attempts to connect to pgsql server. (${PDNS_GPGSQL_HOST})"
+      exit 1
+    fi
+    echo "Creating database (if it does not already exist)"
+    if [[ -z "$(echo "SELECT 1 FROM pg_database WHERE datname = '$PGSQL_DB'" | $PGSQLCMD -t)" ]]; then
+      echo "CREATE DATABASE $PGSQL_DB;" | $PGSQLCMD
+    fi
+    PGSQLCMD="$PGSQLCMD $PGSQL_DB"
+    if [[ -z "$(printf '\dt' | $PGSQLCMD -qAt)" ]]; then
+      echo Initializing Database
+      cat /etc/pdns/sql/schema.pgsql.sql | $PGSQLCMD
+      INITIAL_DB_VERSION=$PGSQL_VERSION
+    fi
+    if [ "$AUTO_SCHEMA_MIGRATION" == "yes" ]; then
+      # init version database if necessary
+      if [[ -z "$(echo "SELECT to_regclass('public.$SCHEMA_VERSION_TABLE');" | $PGSQLCMD -qAt)" ]]; then
+        [ -z "$INITIAL_DB_VERSION" ] && >&2 echo "Error: INITIAL_DB_VERSION is required when you use AUTO_SCHEMA_MIGRATION for the first time" && exit 1
+        echo "CREATE TABLE $SCHEMA_VERSION_TABLE (id SERIAL PRIMARY KEY, version VARCHAR(255) DEFAULT NULL)" | $PGSQLCMD
+        echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$INITIAL_DB_VERSION');" | $PGSQLCMD
+        echo "Initialized schema version to $INITIAL_DB_VERSION"
+      fi
+      # do the database upgrade
+      while true; do
+        current="$(echo "SELECT version FROM $SCHEMA_VERSION_TABLE ORDER BY id DESC LIMIT 1;" | $PGSQLCMD -qAt)"
+        if [ "$current" != "$PGSQL_VERSION" ]; then
+          filename=/etc/pdns/sql/${current}_to_*_schema.pgsql.sql
+          echo "Applying Update $(basename $filename)"
+          $PGSQLCMD < $filename
+          current=$(basename $filename | sed -n 's/^[0-9.]\+_to_\([0-9.]\+\)_.*$/\1/p')
+          echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$current');" | $PGSQLCMD
+        else
+          break
+        fi
+      done
+    fi
+  fi
+
+}
 
 # --help, --version
 [ "$1" = "--help" ] || [ "$1" = "--version" ] && exec pdns_server $1
 # treat everything except -- as exec cmd
 [ "${1:0:2}" != "--" ] && exec "$@"
 
-# Add backward compatibility
+# Initialize values that might be stored in a file -----------------------------
+docker_setup_env
+
+# print pdns image info --------------------------------------------------------
+echo "> PowerDNS started on: $(date)"
+
+# Add backward compatibility ---------------------------------------------------
 [[ "$MYSQL_AUTOCONF" == false ]] && AUTOCONF=false
 
-# Set credentials to be imported into pdns.conf
+# Set credentials to be imported into pdns.conf --------------------------------
 case "$AUTOCONF" in
   mysql)
     export PDNS_LOAD_MODULES=$PDNS_LOAD_MODULES,libgmysqlbackend.so
@@ -95,146 +284,23 @@ case "$AUTOCONF" in
   ;;
 esac
 
-MYSQLCMD="mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASS -r -N"
-PGSQLCMD="psql --host=$PGSQL_HOST --username=$PGSQL_USER"
-SQLITECMD="sqlite3 $PDNS_GSQLITE3_DATABASE"
+# print pdns image info --------------------------------------------------------
+echo "> PowerDNS environment variables:"
+env | grep PDNS_ | grep -v PASS
 
-# wait for Database come ready
-isDBup () {
-  case "$PDNS_LAUNCH" in
-    gmysql)
-      echo "SHOW STATUS" | $MYSQLCMD 1>/dev/null
-      echo $?
-    ;;
-    gpgsql)
-      echo "SELECT 1" | $PGSQLCMD 1>/dev/null
-      echo $?
-    ;;
-    *)
-      echo 0
-    ;;
-  esac
-}
-
-RETRY=10
-until [ `isDBup` -eq 0 ] || [ $RETRY -le 0 ] ; do
-  echo "Waiting for database to come up"
-  sleep 5
-  RETRY=$(expr $RETRY - 1)
-done
-if [ $RETRY -le 0 ]; then
-  if [[ "$MYSQL_HOST" ]]; then
-    >&2 echo Error: Could not connect to Database on $MYSQL_HOST:$MYSQL_PORT
-    exit 1
-  elif [[ "$PGSQL_HOST" ]]; then
-    >&2 echo Error: Could not connect to Database on $PGSQL_HOST:$PGSQL_PORT
-    exit 1
-  fi
-fi
-
-# init database and migrate database if necessary
+# init database and migrate database if necessary ------------------------------
 case "$PDNS_LAUNCH" in
   gmysql)
-    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;" | $MYSQLCMD
-    MYSQLCMD="$MYSQLCMD $MYSQL_DB"
-    if [ "$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = \"$MYSQL_DB\";" | $MYSQLCMD)" -le 1 ]; then
-      echo Initializing Database
-      cat /etc/pdns/sql/schema.mysql.sql | $MYSQLCMD
-      INITIAL_DB_VERSION=$MYSQL_VERSION
-    fi
-    if [ "$AUTO_SCHEMA_MIGRATION" == "yes" ]; then
-      # init version database if necessary
-      if [ "$(echo "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = \"$MYSQL_DB\" and table_name = \"$SCHEMA_VERSION_TABLE\";" | $MYSQLCMD)" -eq 0 ]; then
-        [ -z "$INITIAL_DB_VERSION" ] && >&2 echo "Error: INITIAL_DB_VERSION is required when you use AUTO_SCHEMA_MIGRATION for the first time" && exit 1
-        echo "CREATE TABLE $SCHEMA_VERSION_TABLE (id INT AUTO_INCREMENT primary key NOT NULL, version VARCHAR(255) NOT NULL) Engine=InnoDB CHARACTER SET 'latin1';" | $MYSQLCMD
-        echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$INITIAL_DB_VERSION');" | $MYSQLCMD
-        echo "Initialized schema version to $INITIAL_DB_VERSION"
-      fi
-      # do the database upgrade
-      while true; do
-        current="$(echo "SELECT version FROM $SCHEMA_VERSION_TABLE ORDER BY id DESC LIMIT 1;" | $MYSQLCMD)"
-        if [ "$current" != "$MYSQL_VERSION" ]; then
-          filename=/etc/pdns/sql/${current}_to_*_schema.mysql.sql
-          echo "Applying Update $(basename $filename)"
-          $MYSQLCMD < $filename
-          current=$(basename $filename | sed -n 's/^[0-9.]\+_to_\([0-9.]\+\)_.*$/\1/p')
-          echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$current');" | $MYSQLCMD
-        else
-          break
-        fi
-      done
-    fi
-    # Run custom mysql post-init sql scripts
-    if [ -d "/etc/pdns/mysql-postinit" ]; then
-      for SQLFILE in $(ls -1 /etc/pdns/mysql-postinit/*.sql | sort) ; do
-        echo Source $SQLFILE
-        cat $SQLFILE | $MYSQLCMD
-      done
-    fi
+    echo "> MySQL. Preparing mysql backend ..."
+    init_mysql
   ;;
   gpgsql)
-    if [[ -z "$(echo "SELECT 1 FROM pg_database WHERE datname = '$PGSQL_DB'" | $PGSQLCMD -t)" ]]; then
-      echo "CREATE DATABASE $PGSQL_DB;" | $PGSQLCMD
-    fi
-    PGSQLCMD="$PGSQLCMD $PGSQL_DB"
-    if [[ -z "$(printf '\dt' | $PGSQLCMD -qAt)" ]]; then
-      echo Initializing Database
-      cat /etc/pdns/sql/schema.pgsql.sql | $PGSQLCMD
-      INITIAL_DB_VERSION=$PGSQL_VERSION
-    fi
-    if [ "$AUTO_SCHEMA_MIGRATION" == "yes" ]; then
-      # init version database if necessary
-      if [[ -z "$(echo "SELECT to_regclass('public.$SCHEMA_VERSION_TABLE');" | $PGSQLCMD -qAt)" ]]; then
-        [ -z "$INITIAL_DB_VERSION" ] && >&2 echo "Error: INITIAL_DB_VERSION is required when you use AUTO_SCHEMA_MIGRATION for the first time" && exit 1
-        echo "CREATE TABLE $SCHEMA_VERSION_TABLE (id SERIAL PRIMARY KEY, version VARCHAR(255) DEFAULT NULL)" | $PGSQLCMD
-        echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$INITIAL_DB_VERSION');" | $PGSQLCMD
-        echo "Initialized schema version to $INITIAL_DB_VERSION"
-      fi
-      # do the database upgrade
-      while true; do
-        current="$(echo "SELECT version FROM $SCHEMA_VERSION_TABLE ORDER BY id DESC LIMIT 1;" | $PGSQLCMD -qAt)"
-        if [ "$current" != "$PGSQL_VERSION" ]; then
-          filename=/etc/pdns/sql/${current}_to_*_schema.pgsql.sql
-          echo "Applying Update $(basename $filename)"
-          $PGSQLCMD < $filename
-          current=$(basename $filename | sed -n 's/^[0-9.]\+_to_\([0-9.]\+\)_.*$/\1/p')
-          echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$current');" | $PGSQLCMD
-        else
-          break
-        fi
-      done
-    fi
+    echo "> PGSQL. Preparing pgsql backend ..."
+    init_pgsql
   ;;
   gsqlite3)
-    if [[ ! -f "$PDNS_GSQLITE3_DATABASE" ]]; then
-      install -D -d -o pdns -g pdns -m 0755 $(dirname $PDNS_GSQLITE3_DATABASE)
-      cat /etc/pdns/sql/schema.sqlite3.sql | $SQLITECMD
-      chown pdns:pdns $PDNS_GSQLITE3_DATABASE
-      INITIAL_DB_VERSION=$SQLITE_VERSION
-    fi
-    if [ "$AUTO_SCHEMA_MIGRATION" == "yes" ]; then
-      # init version database if necessary
-      if [[ "$(echo "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='$SCHEMA_VERSION_TABLE';" | $SQLITECMD)" -eq 0 ]]; then
-        [ -z "$INITIAL_DB_VERSION" ] && >&2 echo "Error: INITIAL_DB_VERSION is required when you use AUTO_SCHEMA_MIGRATION for the first time" && exit 1
-        echo "CREATE TABLE $SCHEMA_VERSION_TABLE (id INTEGER PRIMARY KEY, version VARCHAR(255) NOT NULL)" | $SQLITECMD
-        echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$INITIAL_DB_VERSION');" | $SQLITECMD
-        echo "Initialized schema version to $INITIAL_DB_VERSION"
-      fi
-      # do the database upgrade
-      while true; do
-        current="$(echo "SELECT version FROM $SCHEMA_VERSION_TABLE ORDER BY id DESC LIMIT 1;" | $SQLITECMD)"
-        if [ "$current" != "$SQLITE_VERSION" ]; then
-          filename=/etc/pdns/sql/${current}_to_*_schema.sqlite3.sql
-          echo "Applying Update $(basename $filename)"
-          $SQLITECMD < $filename
-          current=$(basename $filename | sed -n 's/^[0-9.]\+_to_\([0-9.]\+\)_.*$/\1/p')
-          echo "INSERT INTO $SCHEMA_VERSION_TABLE (version) VALUES ('$current');" | $SQLITECMD
-        else
-          break
-        fi
-      done
-
-    fi
+    echo "> SQLITE. Preparing sqlite backend ..."
+    init_sqlite3
   ;;
 esac
 
@@ -248,13 +314,48 @@ printenv | grep ^PDNS_ | cut -f2- -d_ | while read var; do
   (grep -qE "^[# ]*$var=.*" /etc/pdns/pdns.conf && sed -r -i "s#^[# ]*$var=.*#$var=$val#g" /etc/pdns/pdns.conf) || echo "$var=$val" >> /etc/pdns/pdns.conf
 done
 
-# environment hygiene
+# environment hygiene ----------------------------------------------------------
 for var in $(printenv | cut -f1 -d= | grep -v -e HOME -e USER -e PATH ); do unset $var; done
 export TZ=UTC LANG=C LC_ALL=C
 
-# prepare graceful shutdown
-trap "pdns_control quit" SIGHUP SIGINT SIGTERM
+# START PDNS -------------------------------------------------------------------
+echo "> Starting pdns ..."
+if [ $# -eq 0 ]; then
+  echo "> Executing as uid [$(/usr/bin/id -u)]: pdns_server --daemon=no --write-pid=yes"
+  pdns_server --daemon=no --write-pid=yes &
+else
+  echo "> Executing as uid [$(/usr/bin/id -u)]: pdns_server --daemon=no --write-pid=yes ${@}"
+  pdns_server --daemon=no --write-pid=yes "$@" &
+fi
 
-pdns_server "$@" &
+# SIGNAL HANDLING --------------------------------------------------------------
+PDNS_PID_FILE=/tmp/pdns.pid
+
+shutdown_pdns() {
+  echo "!! Recieved signal to gracefully shutdown pdns"
+  local pid=$(cat ${PDNS_PID_FILE})
+  pdns_control quit
+  # wait
+  wait_for_pid_to_exit ${pid}
+  exit 0
+}
+
+reload_pdns() {
+  echo "!! Recieved signal to reload pdns"
+  pdns_control cycle
+  wait $(cat ${PDNS_PID_FILE})
+}
+
+wait_for_pid_to_exit() {
+  local pid=${1}
+  echo "waiting for pid [${pid}] to exit ..."
+  while [ -d /proc/${pid} ]; do
+    echo "pid [${pid}] still exists. waiting ..."
+    sleep 1
+  done
+}
+
+trap "reload_pdns" SIGHUP
+trap "shutdown_pdns" SIGINT SIGTERM
 
 wait
